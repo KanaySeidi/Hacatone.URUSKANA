612dc63597f800287d4aa423d3353d95cfea2c50
Urustom <Urustom@Urustoms-MacBook-Pro.local>
2021-12-24 11:33:26 +0600

    добавил чутка функционала

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(() => {
+  const API = "http://localhost:8000/tweets";
+
+  const tweetBtn = $("#tweet-btn");
+  const tweetInp = $("#tweet-inp");
+
+  async function addTweet() {
+    try {
+      await axios.post(API, {
+        tittle: tweetInp.val().trim(),
+        authorId: 1,
+        likes: 0,
+        reTweets: 0,
+        comments: [
+          { id: 1, comment: "Good!" },
+          { id: 2, comment: "WoW!" },
+          { id: 3, comment: "Nice!" },
+        ],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  tweetBtn.on("click", addTweet);
+
+  async function renderTweet() {
+    $(".tweets").html("");
+    const response = await axios(API);
+    response.data.forEach(() => {
+      $(".tweets").append(`
+        <div class="card">
+  <div class="card-header">
+    Tweet #${element.id}
+  </div>
+  <div class="card-body">
+    <p class="card-text">${element.title}</p>
+    <a href="#" class="like-btn btn btn-outline-primary">Like ${element.likes}</a>
+  </div>
+</div>
+        `);
+    });
+  }
+  renderTweet();
+});
